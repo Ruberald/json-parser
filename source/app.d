b5a8a2a592c33b1a866c3f6395dd97e869d586d8
Ruberald <sajidhossain909@gmail.com>
2024-09-14 15:49:03 +0530

    Can parse string values

--- source/app.d
+++ source/app.d
@@ -4,16 +4,46 @@ import std.string;
 import core.stdc.errno;
 import std.exception;
 
-void parse_input(string input) {
-    if (input.length == 0) {
+string parse_string(string s) {
+    s = s.strip;
+    if (s.length < 2) {
         stderr.writeln("ERROR: Empty string");
-        return;
+        return "";
     }
 
-    if (input[0] != '{' || input[input.length - 1] != '}') {
+    if (!s.startsWith("\"") || !s.endsWith("\"") ) {
         stderr.writeln("ERROR: Wrong format");
-        return;
+        return "";
     }
+
+    return strip(s, "\"");
+}
+
+string[string] parse_values(string[] lines) {
+    string[string] map;
+
+    foreach(line; lines) {
+        auto split_line = split(line, ":");
+
+        map[parse_string(split_line[0])] = parse_string(split_line[1]);
+    }
+
+    return map;
+}
+
+string[] parse_input(string input) {
+    if (input.length < 2) {
+        stderr.writeln("ERROR: Empty string");
+        return [];
+    }
+
+    if (!input.startsWith("{") || !input.endsWith("}") ) {
+        stderr.writeln("ERROR: Wrong format");
+        return [];
+    }
+
+    input = input.strip("{}");
+    return input.split(",");
 }
 
 string read_input(string file_name) {
@@ -59,9 +89,10 @@ int main(string[] args)
     }
 
     input = input.strip;
-    
-    parse_input(input);
-    writeln(input);
+    auto result = parse_input(input);
+    auto values = parse_values(result);
+    writeln(result);
+    writeln(values);
 
     return 0;
 }
